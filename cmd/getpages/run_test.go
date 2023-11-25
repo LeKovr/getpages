4cf3b3aa04ad6d3f56078ce5a143f4a4a8382d2f
Alexey Kovrizhkin <lekovr@gmail.com>
2023-11-25 23:05:31 +0300

    fix: new files

---
+++ cmd/getpages/run_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/LeKovr/getpages/pkgtest"
+)
+
+func TestMain(t *testing.T) {
+	srv := httptest.NewServer(
+		http.HandlerFunc(pkgtest.Handler),
+	)
+	defer srv.Close()
+
+	file := pkgtest.FillSource(t, srv.URL)
+	defer func() {
+		err := os.Remove(file)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+	ctx := context.Background()
+	var c int
+	a := os.Args
+	os.Args = []string{"test", "-source", file, "-timeout", "5ms"}
+	Run(ctx, func(code int) { c = code })
+	if c != 0 {
+		t.Errorf("expected 0, got %d", c)
+	}
+	os.Args = a
+}
